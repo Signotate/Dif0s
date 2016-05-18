ee4334a5ec89eb89fef9d832e4094ad875c075a1
Greg Clark <greg@eigendomain.com>
2016-05-17 23:24:03 -0400

    Initialize repository

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'DeBiI Sign Language Markup Tool',
+    version = '0.0.1',
+    packages = find_packages(),
+)
