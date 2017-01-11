46fe4e0d32058600604fd696d4f506be68b9ca99
Greg Clark <greg@eigendomain.com>
2017-01-11 01:48:49 -0500

    Add notice to source files

--- signlangmtk/difos/utils.py
+++ signlangmtk/difos/utils.py
@@ -1,5 +1,23 @@
-from io import BytesIO
-from pkgutil import get_data
+# Sign Language Markup Tool Kit
+# Tools to model, search and create scalable graphic representations of sign
+# language transcripts
+#
+# Copyright (C) 2016, 2017 Greg Clark
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 from gi.repository import GdkPixbuf
 from signlangmtk.util import resource_location
 import signlangmtk.difos.gui
