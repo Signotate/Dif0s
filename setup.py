8f2c7dddff3a45e885820eb2c99d0f005f35231c
Greg Clark <greg@eigendomain.com>
2017-01-11 01:38:42 -0500

    Setup for 0.1a1.dev0

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+
 from setuptools import setup
 from setuptools import find_packages
+from signlangmtk.__about__ import *
+import os
+
 
 setup(
-    name = 'Sign Language Markup Tool Kit',
-    version = '0.1.a0dev',
-    packages = find_packages(),
+    name=__title__,
+    description=__summary__,
+    long_description=__long_summary__,
+    version=__version__,
+    packages=find_packages(),
+
+    package_data={'signlangmtk.difos.gui':
+                  ['*.ui', '*.css', '*.png', '*.ico']},
+    include_package_data=True,
+
+    author=__author__,
+    author_email=__email__,
+    license=__license__,
+
+    entry_points={
+        'gui_scripts': [
+            'dif0s = signlangmtk.difos.__main__:main'
+        ]
+    },
+
+    data_files=[
+        ('share/applications', ['dif0s.desktop']),
+        ('share/icons/hicolor/scalable/apps',
+         ['signlangmtk/difos/gui/dif0s.svg'])
+    ]
 )
